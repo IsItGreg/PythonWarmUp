2d5acab6ce10075d9a6e1d3a65fa6ce07748cdb3
IsItGreg <gsmelkov@gmail.com>
2018-06-05 12:10:11 -0400

    Changed warmup.py to single url

--- warmup.py
+++ warmup.py
@@ -2,19 +2,21 @@ import urllib2
 from bs4 import BeautifulSoup, NavigableString
 
 
-def cleanArticle(urls, number):
-    #urls = ["https://www.voanews.com/a/trump-offers-contradictory-explanation-for-comey-firing/4417821.html"]
-    for i in range(0, len(urls)):
-        tempUrl = urls[i]
-        tempHtml = urllib2.urlopen(tempUrl)
-        soup = BeautifulSoup(tempHtml, 'html.parser')
-        body = soup.find_all('p')
-        with open("url"+"{:0>3d}".format(number)+".txt", "w") as text_file:
-            text_file.write(soup.title.string.encode('ascii', 'ignore') +"\n")
-            for line in body:
-                if line.string != None:
-                    text_file.write(line.string.encode('ascii', 'ignore') +"\n")
-        #print("Done")
+def cleanArticle(url, number):
+    '''
+
+    :param url:
+    :param number:
+    :return article title
+    '''
+    html = urllib2.urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    content = soup.find_all('p')
+    with open("article"+"{:0>3d}".format(number)+".txt", "w") as text_file:
+        text_file.write(soup.title.string.encode('ascii', 'ignore') +"\n")
+        for line in content:
+            if line.string != None:
+                text_file.write(line.string.encode('ascii', 'ignore') +"\n")
     return soup.title.string
 
 
