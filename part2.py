552ff568debaa905601897266f687f23e715129d
IsItGreg <gsmelkov@gmail.com>
2018-06-05 18:07:40 -0400

    Works worse now

--- part2.py
+++ part2.py
@@ -13,59 +13,23 @@ def getArticle(author, soup, num, count):
     tIndexes = []
     newNum = num
     for link in soup.find_all('a'):
-        if link.get('href').startswith("/a/") and len(link.get('href')) > 15 and link.get('href') != tLastLink \
+        if link.get('href').startswith("/a/") and len(link.get('href')) > 16 and link.get('href') != tLastLink \
                 and count < 25:
-            tLinks += [link.get('href')]
+            tLinks += ["https://www.voanews.com/" + link.get('href')]
             tAuthors += [author]
             tLastLink = link.get('href')
-            tTitles += [cleanArticle("https://www.voanews.com" + tLastLink, newNum + count)]
             tIndexes += ["{:0>3d}".format(newNum + count)]
-            count+=1
+            #print(tLastLink)
+            tTitles += [cleanArticle("https://www.voanews.com" + tLastLink, newNum + count)]
+            if tTitles[len(tTitles) - 1] == "emptyfile":
+                tTitles.pop()
+                tIndexes.pop()
+                tAuthors.pop()
+                tLinks.pop()
+
+            else:
+                count+=1
     return tIndexes, tAuthors, tTitles, tLinks, count
-
-urls = ["https://www.voanews.com/author/4365.html",
-        "https://www.voanews.com/author/25810.html",
-        "https://www.voanews.com/author/4345.html",
-        "https://www.voanews.com/author/4511.html",
-        "https://www.voanews.com/author/25555.html",
-        "https://www.voanews.com/author/24557.html",
-        "https://www.voanews.com/author/4349.html",
-        "https://www.voanews.com/author/23467.html"]
-
-k = 0
-links = []
-authors = []
-titles = []
-indexes = []
-lastLink = ""
-for i in range(0, len(urls)):
-    tempHtml = urllib2.urlopen(urls[i])
-    soup = BeautifulSoup(tempHtml, 'html.parser')
-    author = soup.find('h1').string
-    print(author)
-
-    tempIndexes, tempAuthors, tempTitles, tempLinks, j = getArticle(author, soup, k, 0)
-    indexes += tempIndexes
-    authors += tempAuthors
-    titles += tempTitles
-    links += tempLinks
-    print(j)
-
-    print("pag2")
-    tempHtml = urllib2.urlopen(str(urls[i]) + "?page=2")
-    soup = BeautifulSoup(tempHtml, 'html.parser')
-    tempIndexes, tempAuthors, tempTitles, tempLinks, j = getArticle(author, soup, k, j)
-    indexes += tempIndexes
-    authors += tempAuthors
-    titles += tempTitles
-    links += tempLinks
-    k += j
-    print(j)
-
-data = {'Author':authors, 'Title':titles, 'URL':links}
-df = pandas.DataFrame(data, index=indexes).rename_axis("Text #", axis=1)
-df.to_csv('urlMetadata.txt', sep='\t', encoding='utf-8')
-
 
 def main():
     urls = ["https://www.voanews.com/author/4365.html",
@@ -82,7 +46,6 @@ def main():
     authors = []
     titles = []
     indexes = []
-    lastLink = ""
     for i in range(0, len(urls)):
         tempHtml = urllib2.urlopen(urls[i])
         soup = BeautifulSoup(tempHtml, 'html.parser')
@@ -96,16 +59,21 @@ def main():
         links += tempLinks
         print(j)
 
-        print("pag2")
-        tempHtml = urllib2.urlopen(str(urls[i]) + "?page=2")
-        soup = BeautifulSoup(tempHtml, 'html.parser')
-        tempIndexes, tempAuthors, tempTitles, tempLinks, j = getArticle(author, soup, k, j)
-        indexes += tempIndexes
-        authors += tempAuthors
-        titles += tempTitles
-        links += tempLinks
-        k += j
-        print(j)
+        for l in range(2, 5):
+            print("pag" + str(l))
+            try:
+                tempHtml = urllib2.urlopen(str(urls[i]) + "?page=" + str(l))
+            except:
+                print("No page number " + str(l))
+                continue
+            soup = BeautifulSoup(tempHtml, 'html.parser')
+            tempIndexes, tempAuthors, tempTitles, tempLinks, j = getArticle(author, soup, k, j)
+            indexes += tempIndexes
+            authors += tempAuthors
+            titles += tempTitles
+            links += tempLinks
+            k += j
+            print(j)
 
     data = {'Author': authors, 'Title': titles, 'URL': links}
     df = pandas.DataFrame(data, index=indexes).rename_axis("Text #", axis=1)
--- warmup.py
+++ warmup.py
@@ -1,22 +1,61 @@
 import urllib2
-from bs4 import BeautifulSoup, NavigableString
-
+import string
+from bs4 import BeautifulSoup
 
 def cleanArticle(url, number):
     '''
 
     :param url:
     :param number:
-    :return article title
+    :return article title:
     '''
-    html = urllib2.urlopen(url)
+    try:
+        html = urllib2.urlopen(url)
+    except:
+        return "emptyfile"
     soup = BeautifulSoup(html, 'html.parser')
-    content = soup.find_all('p')
+    content = soup.find_all("p")
+    count = 0
+    keepGoing = True
+
+    print(url)
     with open("article"+"{:0>3d}".format(number)+".txt", "w") as text_file:
-        text_file.write(soup.title.string.encode('ascii', 'ignore') +"\n")
+        #text_file.write(soup.title.string.encode('ascii', 'ignore') +"\n")
         for line in content:
-            if line.string != None:
-                text_file.write(line.string.encode('ascii', 'ignore') +"\n")
+            if keepGoing == True:
+                if line.string == None:
+                    for text in line.stripped_strings:
+                        if len(text) >= 15 and text != "No media source currently available":
+                            #print(str(text.encode('ascii', 'ignore')) + " : " + str(str(text.encode('ascii', 'ignore')).count(' ') + 1))
+                            if str(text.encode('ascii', 'ignore')).count(' ') + 1 > 5:
+                                count += str(text.encode('ascii', 'ignore')).count(' ') + 1
+                            else:
+                                text_file.write(text.encode('ascii', 'ignore').upper() + "\n")
+                                break
+                            if count <=340:
+                                text_file.write(text.encode('ascii', 'ignore') +"\n")
+                            else:
+                                keepGoing = False
+                else:
+                    #print(str(line.string.encode('ascii', 'ignore')) + " :: " + str(str(line.string.encode('ascii', 'ignore')).count(' ') + 1))
+                    if str(text.encode('ascii', 'ignore')).count(' ') + 1 > 5:
+                        count += str(line.string.encode('ascii', 'ignore')).count(' ') + 1
+                    else:
+                        text_file.write(text.encode('ascii', 'ignore').upper() + "\n")
+                        continue
+                    if count <= 340:
+                        text_file.write(line.string.encode('ascii', 'ignore') +"\n")
+                    else:
+                        keepGoing = False
+            else:
+                break
+
+    print (str(number) + " " + str(count))
+
+    if count < 300:
+        #clear file
+        return "emptyfile"
+
     return soup.title.string
 
 
