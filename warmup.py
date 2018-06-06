fd2343b52c0af029e1077b960d583662b4af3abe
IsItGreg <gsmelkov@gmail.com>
2018-06-06 10:31:44 -0400

    Check for duplicate phrases added along with exclusion of common, non-article phrases

--- warmup.py
+++ warmup.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 def stringToFile(string, file):
     if len(string) >= 15:
         if string.count(' ') + 1 <= 5:
+            if any(string.upper() in s for s in ["PRINT", "NO MEDIA SOURCE CURRENTLY AVAILABLE"]):
+                return 0
             file.write(string.upper() + "\n")
             return 0
         else:
@@ -21,6 +23,7 @@ def cleanArticle(url, number):
     content = soup.find_all("p")
     count = 0
     keepGoing = True
+    lastLine = ""
     #print(url)
     with open("article"+"{:0>3d}".format(number)+".txt", "w") as text_file:
         for line in content:
@@ -28,14 +31,20 @@ def cleanArticle(url, number):
                 if line.string == None:
                     for text in line.stripped_strings:
                         newText = text.encode('ascii', 'ignore')
+                        if newText == lastLine:
+                            continue
                         if count + newText.count(' ') <=340:
                             count += stringToFile(newText, text_file)
+                            lastLine = newText
                         else:
                             keepGoing = False
                 else:
                     newText = line.string.encode('ascii', 'ignore')
-                    if count + newText.count(' ') <= 340:
+                    if newText == lastLine:
+                        continue
+                    elif count + newText.count(' ') <= 340:
                         count += stringToFile(newText, text_file)
+                        lastLine = newText
                     else:
                         keepGoing = False
             else:
